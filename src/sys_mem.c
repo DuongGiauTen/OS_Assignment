bbe5b8cac68492200bc3237fe91a7eed3b678df1
gitTest <duongtranminh253@gmail.com>
2025-11-18 16:20:02 +0700

    finish vm-mm.c and syscalxxxhandler, add testcase...

--- src/sys_mem.c
+++ src/sys_mem.c
@@ -26,11 +26,18 @@ int __sys_memmap(struct krnl_t *krnl, uint32_t pid, struct sc_regs* regs)
 {
    int memop = regs->a1;
    BYTE value;
+
+
+
    
    /* TODO THIS DUMMY CREATE EMPTY PROC TO AVOID COMPILER NOTIFY 
     *      need to be eliminated
 	*/
-   struct pcb_t *caller = malloc(sizeof(struct pcb_t));
+
+//-------------------==========================---------------------------
+   //struct pcb_t *caller = malloc(sizeof(struct pcb_t));
+//-------------------==========================---------------------------
+
 
    /*
     * @bksysnet: Please note in the dual spacing design
@@ -45,7 +52,31 @@ int __sys_memmap(struct krnl_t *krnl, uint32_t pid, struct sc_regs* regs)
     /* TODO Maching and marking the process */
     /* user process are not allowed to access directly pcb in kernel space of syscall */
     //....
-	
+
+
+
+    /* --- [UPDATED] TÌM PROCESS THỰC SỰ (Thay vì tạo Dummy) --- */
+   struct pcb_t *caller = NULL;
+   struct queue_t *running_list = krnl->running_list;
+
+   // Duyệt danh sách running_list để tìm process có PID trùng khớp
+   if (running_list != NULL) {
+       // Lưu ý: Sử dụng vòng lặp for vì queue.h định nghĩa queue là mảng proc[]
+       for (int i = 0; i < running_list->size; i++) {
+           struct pcb_t *proc = running_list->proc[i];
+           if (proc != NULL && proc->pid == pid) {
+               caller = proc;
+               break; // Đã tìm thấy
+           }
+       }
+   }
+
+   // Nếu không tìm thấy process nào khớp PID -> Báo lỗi
+   if (caller == NULL) {
+       return -1;
+   }
+   /* ---------------------------------------------------------- */
+   
    switch (memop) {
    case SYSMEM_MAP_OP:
             /* Reserved process case*/
